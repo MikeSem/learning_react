014136845489070bd30c2f20d87d00d1f60ad249
Mike <semenyuk.nazar16@gmail.com>
2017-10-30 16:49:59 +0200

    Added dropdown with local state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+import Dropdown from './Dropdown';
+
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
-  }
+    return (
+      <div>
+   		<Dropdown />
+      </div>
+	  );
+    }
 
 }
 
